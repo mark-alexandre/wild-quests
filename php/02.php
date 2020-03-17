6ce62ebccd893be7665e954eca2bbe98115a127d
Moxymore67 <61732473+Moxymore67@users.noreply.github.com>
2020-03-17 19:03:36 +0100

    Create 02.php

---
+++ php/02.php
@@ -0,0 +1,11 @@
+<?php
+
+$filmTitle = (string) "Indiana Jones and the Last Crusade";
+$watchedFilm = (bool) TRUE;
+$releaseYear = (int) 1989;
+$rateValue = (float) 8.2;
+
+var_dump($filmTitle);
+var_dump($watchedFilm);
+var_dump($releaseYear);
+var_dump($rateValue);
