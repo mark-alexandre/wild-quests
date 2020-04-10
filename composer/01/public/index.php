39f88cb116a5d077351025411231cd2d6836e423
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-04-10 19:00:03 +0200

    quest

--- composer/01/public/index.php
+++ composer/01/public/index.php
@@ -2,6 +2,7 @@
 require '../vendor/autoload.php';
 use App\Wcs\Hello;
 
+/* NOTE : vendor folder in .gitignore, so not uploaded in this repo */
 $var = new Hello();
 echo $var->talk('') . '<br>'; //With an empty string
 echo $var->talk('Hello World !') . '<br>'; //With a proper string
--- composer/02/public/index.php
+++ composer/02/public/index.php
@@ -9,5 +9,6 @@ echo $var->talk('') . '<br>'; //With an empty string
 echo $var->talk('Hello World !') . '<br>'; //With a proper string
 
 // Quest #2
+/* NOTE : vendor folder in .gitignore, so not uploaded in this repo */
 $foo = new SayHello;
 echo $foo::world();
