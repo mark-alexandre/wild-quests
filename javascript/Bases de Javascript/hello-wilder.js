4db3f3c1f02605bc0a7a1dd7b26fa749fddd2f31
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-05-01 22:14:33 +0200

    quest

---
+++ javascript/Bases de Javascript/hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+let favoriteFilm = 'Requiem for a dream';
+let releaseYear = 2000;
+let director = 'Darren Aronofsky';
+let message = favoriteFilm + ', réalisé par ' + director + ', est sorti en ' + releaseYear + '.';
+
+// Display a popup
+alert(message);
