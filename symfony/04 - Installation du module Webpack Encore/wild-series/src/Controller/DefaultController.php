b1fc4f44e3d102c3c10e0334e50d39577d164969
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-05-11 12:23:52 +0200

    quest

---
+++ symfony/04 - Installation du module Webpack Encore/wild-series/src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('/home.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
